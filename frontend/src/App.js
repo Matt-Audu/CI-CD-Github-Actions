07e2cac45c4936f84f81c7b42d5a8568b61f9379
Matt-Audu <simplymailmatthew7@gmail.com>
2025-04-18 22:21:09 +0100

    testing fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 function App() {
   const [destinations, setDestinations] = useState([]);
   const [country, setCountry] = useState('');
-  const [country, setCountry] = useState('');
 
   useEffect(() => {
     fetchDestinations();
